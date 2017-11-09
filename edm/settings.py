b55b271c4390e3f4d0931309b9876e7ba2d89fc0
vishnuvijay <vishnupdp@gmail.com>
2017-11-09 01:53:22 -0700

    Update settings.py

--- edm/settings.py
+++ edm/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -136,7 +136,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, '/static/'),
 #    os.path.join(BASE_DIR, 'staticfiles'),
  #   os.path.join(BASE_DIR, 'static'),
   #  os.path.join(BASE_DIR,'edm', 'staticfiles'),
