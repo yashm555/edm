4a260fd95d7831cb41c8e580c477657322d8272c
vishnuvijay <vishnupdp@gmail.com>
2017-11-09 02:09:03 -0700

    Update settings.py

--- edm/settings.py
+++ edm/settings.py
@@ -89,7 +89,6 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(BASE_DIR, 'staticfiles'),
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR,'edm', 'staticfiles'),
     os.path.join(BASE_DIR,'edm', 'static'),
